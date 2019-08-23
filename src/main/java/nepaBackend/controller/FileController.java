2a18f9b473d14aa4d80417645c94ccf848365db8
jvinal <deathpizza@hotmail.com>
2019-08-23 13:19:15 -0700

    Test refactor

--- src/main/java/nepaBackend/controller/FileController.java
+++ src/main/java/nepaBackend/controller/FileController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,22 +20,24 @@ public class FileController {
 
 	@CrossOrigin
 	@RequestMapping(value = "/downloadFile", method = RequestMethod.GET)
-	public void downLoadFile(HttpServletRequest request, HttpServletResponse response) {
+	public ResponseEntity<Void> downloadFile(HttpServletRequest request, HttpServletResponse response) {
 	    try {
-	        String fileName = request.getParameter("fileName");
-	        File file = new File("mis-jvinaldbl1.catnet.arizona.edu" +"//"+fileName);
+	        String filename = request.getParameter("filename");
+	        File file = new File("mis-jvinaldbl1.catnet.arizona.edu/test/"+filename);
 	        InputStream in = new BufferedInputStream(new FileInputStream(file));
 
 	        response.setContentType("application/xlsx");
-	        response.setHeader("Content-Disposition", "attachment; filename="+fileName+".xlsx"); 
+	        response.setHeader("Content-Disposition", "attachment; filename="+filename+".txt"); 
 
 
 	        ServletOutputStream out = response.getOutputStream();
 	        IOUtils.copy(in, out);
 	        response.flushBuffer();
 	    } catch (Exception e) {
-	        e.printStackTrace();
+//	        e.printStackTrace();
+    		return new ResponseEntity<Void>(HttpStatus.SERVICE_UNAVAILABLE);
 	    }
+		return new ResponseEntity<Void>(HttpStatus.I_AM_A_TEAPOT);
 	}
 	
 }
