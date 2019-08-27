b013ca8378d46428564ee36b8b7f276844f41a7a
jvinal <deathpizza@hotmail.com>
2019-08-27 16:26:25 -0700

    Update FileController.java

--- src/main/java/nepaBackend/controller/FileController.java
+++ src/main/java/nepaBackend/controller/FileController.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,15 +18,17 @@ public class FileController {
 
 	@CrossOrigin
 	@RequestMapping(path = "/downloadFile", method = RequestMethod.GET)
-	public ResponseEntity<Void> downloadFile(HttpServletRequest request, HttpServletResponse response) {
+	public void downloadFile(HttpServletRequest request, HttpServletResponse response) {
 	    try {
 	        String filename = request.getParameter("filename");
 //	        File file = new File("mis-jvinaldbl1.catnet.arizona.edu/test/"+filename);
 	        File file = new File("mis-jvinaldbl1.catnet.arizona.edu/test/test.txt");
 	        InputStream in = new BufferedInputStream(new FileInputStream(file));
 
-	        response.setContentType("application/xlsx");
-	        response.setHeader("Content-Disposition", "attachment; filename="+filename+".txt"); 
+//	        response.setContentType("application/xlsx");
+	        response.setContentType("txt/plain");
+//	        response.setHeader("Content-Disposition", "attachment; filename="+filename+".txt"); 
+	        response.setHeader("Content-Disposition", "attachment; filename=test.txt"); 
 
 
 	        ServletOutputStream out = response.getOutputStream();
@@ -36,9 +36,7 @@ public class FileController {
 	        response.flushBuffer();
 	    } catch (Exception e) {
 //	        e.printStackTrace();
-    		return new ResponseEntity<Void>(HttpStatus.SERVICE_UNAVAILABLE);
 	    }
-		return new ResponseEntity<Void>(HttpStatus.I_AM_A_TEAPOT);
 	}
 	
 }
