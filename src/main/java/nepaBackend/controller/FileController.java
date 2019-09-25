cfd1950f2dbbfbce49e05fecedd43777ed6b822b
jvinal <deathpizza@hotmail.com>
2019-09-25 13:14:18 -0700

    FileController cleaning

--- src/main/java/nepaBackend/controller/FileController.java
+++ src/main/java/nepaBackend/controller/FileController.java
@@ -1,14 +1,9 @@
 package nepaBackend.controller;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.servlet.ServletOutputStream;
@@ -16,25 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
+	
+	String dbURL = "http://mis-jvinaldbl1.catnet.arizona.edu:80/test/";
 
-	// TODO: import relevant files, test with big files, etc.
+	// TODO: import relevant files, test deployed
 
 	@CrossOrigin
 	@RequestMapping(path = "/downloadFile", method = RequestMethod.GET)
@@ -42,21 +34,16 @@ public class FileController {
 			@RequestParam(value = "filename") String filename) {
 	    
 		try {
-	    	System.out.println("Activated for: http://mis-jvinaldbl1.catnet.arizona.edu:80/test/" + filename);
-//	        String filename = request.getParameter("filename");
-//	        File file = new File("mis-jvinaldbl1.catnet.arizona.edu/test/"+filename);
-	        URL fileURL = new URL("http://mis-jvinaldbl1.catnet.arizona.edu:80/test/" + filename);
+//	    	System.out.println("Activated for: " + dbURL + filename);
+	    	
+	        URL fileURL = new URL(dbURL + filename);
 	        InputStream in = new BufferedInputStream(fileURL.openStream());
-	        // TODO: Try just saving the file "locally" (to backend VM) to make sure it has permission
-
-//	        response.setContentType("application/xlsx");
+	        
 	        response.addHeader("Content-Disposition", "attachment; filename=" + filename); 
-//	        response.setHeader("Content-Disposition", "attachment; filename="+filename);
-//	        response.setContentType("txt/plain"); 
-
 
 	        ServletOutputStream out = response.getOutputStream();
 	        IOUtils.copy(in, out);
+	        
 	        response.flushBuffer();
 	        return new ResponseEntity<String>("Whew", HttpStatus.ACCEPTED);
 	    } catch (Exception e) {
@@ -65,37 +52,9 @@ public class FileController {
 	    	e.printStackTrace(pw);
 	    	e.printStackTrace();
 	    	String sStackTrace = sw.toString();
+	    	// TODO: Probably don't provide stack trace outside testing
 	        return new ResponseEntity<String>(sStackTrace, HttpStatus.NOT_FOUND);
 	    }
 	}
 
-//	@CrossOrigin
-//	@RequestMapping(path = "/download", method = RequestMethod.GET)
-//	public void download( HttpServletResponse response ) throws IOException {
-//    	System.out.println("Activated2");
-//
-//	    WebClient webClient = WebClient.create();
-//
-//	    URI fileUrl = null;
-//		try {
-//			fileUrl = new URI("http://mis-jvinaldbl1.catnet.arizona.edu:80/test/test.txt");
-//		} catch (URISyntaxException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//	    // Request service to get file data
-//	    Flux<DataBuffer> fileDataStream = webClient.get()
-//	            .uri( fileUrl )
-//	            .accept( MediaType.APPLICATION_OCTET_STREAM )
-//	            .retrieve()
-//	            .bodyToFlux( DataBuffer.class );
-//
-//	    // Streams the stream from response instead of loading it all in memory
-//	    DataBufferUtils.write( fileDataStream, response.getOutputStream() )
-//	            .map( DataBufferUtils::release )
-//	            .then()
-//	            .block();
-//	}
-	
 }
