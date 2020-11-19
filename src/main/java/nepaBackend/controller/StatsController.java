a3e0e70c45b4e25cbfd77eecedb45322d34ecd3f
josh <josh@whew>
2020-11-19 16:52:31 -0700

    Stats routes

---
+++ src/main/java/nepaBackend/controller/StatsController.java
@@ -0,0 +1,105 @@
+package nepaBackend.controller;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import nepaBackend.DocRepository;
+
+@RestController
+@RequestMapping("/stats")
+public class StatsController {
+
+	private DocRepository docRepository;
+	
+	public StatsController(DocRepository docRepository) {
+		this.docRepository = docRepository;
+	}
+
+	@CrossOrigin
+	@GetMapping(path = "/type_count", 
+	produces = "application/json", 
+	headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<List<Object>> getTypeCount() {
+		try {
+			return new ResponseEntity<List<Object>>(docRepository.getTypeCount(), HttpStatus.OK);
+		} catch (Exception e) {
+			//	if (log.isDebugEnabled()) {
+			//		log.debug(e);
+			//	}
+//			e.printStackTrace();
+			return new ResponseEntity<List<Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+
+	@CrossOrigin
+	@GetMapping(path = "/downloadable_count_type", 
+	produces = "application/json", 
+	headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<List<Object>> getDownloadableCountByType() {
+		try {
+			return new ResponseEntity<List<Object>>(docRepository.getDownloadableCountByType(), HttpStatus.OK);
+		} catch (Exception e) {
+			//	if (log.isDebugEnabled()) {
+			//		log.debug(e);
+			//	}
+//			e.printStackTrace();
+			return new ResponseEntity<List<Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@CrossOrigin
+	@GetMapping(path = "/draft_final_count_year", 
+	produces = "application/json", 
+	headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<List<Object>> getDraftFinalCountByYear() {
+		try {
+			return new ResponseEntity<List<Object>>(docRepository.getDraftFinalCountByYear(), HttpStatus.OK);
+		} catch (Exception e) {
+			//	if (log.isDebugEnabled()) {
+			//		log.debug(e);
+			//	}
+//			e.printStackTrace();
+			return new ResponseEntity<List<Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@CrossOrigin
+	@GetMapping(path = "/draft_final_count_state", 
+	produces = "application/json", 
+	headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<List<Object>> getDraftFinalCountByState() {
+		try {
+			return new ResponseEntity<List<Object>>(docRepository.getDraftFinalCountByState(), HttpStatus.OK);
+		} catch (Exception e) {
+			//	if (log.isDebugEnabled()) {
+			//		log.debug(e);
+			//	}
+//			e.printStackTrace();
+			return new ResponseEntity<List<Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@CrossOrigin
+	@GetMapping(path = "/draft_final_count_agency", 
+	produces = "application/json", 
+	headers = "Accept=application/json")
+	public @ResponseBody ResponseEntity<List<Object>> getDraftFinalCountByAgency() {
+		try {
+			return new ResponseEntity<List<Object>>(docRepository.getDraftFinalCountByAgency(), HttpStatus.OK);
+		} catch (Exception e) {
+			//	if (log.isDebugEnabled()) {
+			//		log.debug(e);
+			//	}
+//			e.printStackTrace();
+			return new ResponseEntity<List<Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+}
