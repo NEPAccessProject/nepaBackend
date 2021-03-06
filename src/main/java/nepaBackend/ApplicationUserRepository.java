fad97ba2f545ccb21033418f6bf1f86d7fc2bbe2
josh <josh@whew>
2021-03-05 17:29:28 -0700

    New approver role and get users, approval routes

--- src/main/java/nepaBackend/ApplicationUserRepository.java
+++ src/main/java/nepaBackend/ApplicationUserRepository.java
@@ -1,10 +1,18 @@
 package nepaBackend;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import nepaBackend.model.ApplicationUser;
 
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
     ApplicationUser findByUsername(String username);
     ApplicationUser findByEmail(String email);
+    
+
+	@Query(value = "SELECT id,username,active,email,verified,first_name,last_name FROM application_user a",
+			nativeQuery = true)
+    List<Object> findLimited();
 }
