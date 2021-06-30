bb6e565354ba900577acbe1ac51e6a65b03263dd
josh <josh@whew>
2021-06-29 18:27:24 -0700

    Fixed exception bulk deleting matches

--- src/main/java/nepaBackend/EISMatchService.java
+++ src/main/java/nepaBackend/EISMatchService.java
@@ -2,6 +2,8 @@ package nepaBackend;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,7 +46,21 @@ public class EISMatchService {
 		return matchRepository.findAllByDocument2(id);
 	}
 
+	// this ends up being too big if the caller doesn't split up the lists, so let's split it up
+	// into batches in order to utilize deleteInBatch
 	public void deleteInBatch(List<EISMatch> matches) {
-		matchRepository.deleteInBatch(matches);
+		List<List<EISMatch>> batches = getBatches(matches,1000);
+		for(List<EISMatch> batch : batches) {
+			matchRepository.deleteInBatch(batch);
+		}
 	}
+	
+	// so we don't overflow with deleteInBatch()
+	private static <T> List<List<T>> getBatches(List<T> collection, int batchSize) {
+	    return IntStream.iterate(0, i -> i < collection.size(), i -> i + batchSize)
+	            .mapToObj(i -> collection.subList(i, Math.min(i + batchSize, collection.size())))
+	            .collect(Collectors.toList());
+	}
+	
+	
 }
