c703ca7986e7459ac25a7259f1ea5cd3e4219718
jvinal <deathpizza@hotmail.com>
2021-09-08 15:39:38 -0700

    New enum value for process details page navigation

--- src/main/java/nepaBackend/enums/ActionType.java
+++ src/main/java/nepaBackend/enums/ActionType.java
@@ -2,7 +2,7 @@ package nepaBackend.enums;
 
 /** DOWNLOAD_ARCHIVE(0), DOWNLOAD_ONE(1), DETAILS_CLICK(2); */
 public enum ActionType {
-	DOWNLOAD_ARCHIVE(0), DOWNLOAD_ONE(1), DETAILS_CLICK(2), UNKNOWN(3);
+	DOWNLOAD_ARCHIVE(0), DOWNLOAD_ONE(1), DETAILS_CLICK(2), UNKNOWN(3), PROCESS_CLICK(4);
 	int val;
 	ActionType(int val){
 		this.val = val;
